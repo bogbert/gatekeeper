faaf69738f1114cab981bcc027ccec103c2a9fcf
Pierre G. Bogossian <pbogossi@dxc.com>
2026-02-27 15:38:09 +0100

    Fix id token retrieval, fix case where it's encrypted+compressed

--- pkg/proxy/handlers/handlers.go
+++ pkg/proxy/handlers/handlers.go
@@ -120,15 +120,21 @@ func RetrieveIDToken(
 	if enableEncryptedToken || forceEncryptedCookie {
 		encrypted = token
 
-		token, err = encryption.DecodeText(token, encryptionKey)
-		if err != nil && enableOptionalEncryption {
-			return encrypted, encrypted, nil
-		}
-
 		if enableCompressToken {
 			token, err = session.DecryptAndDecompressToken(token, encryptionKey)
 			if err != nil {
+				if enableOptionalEncryption {
+					return encrypted, encrypted, nil
+				}
 				return "", "", errors.Join(apperrors.ErrDecryptAndDecompressToken, err)
+			}
+		} else {
+			token, err = encryption.DecodeText(token, encryptionKey)
+			if err != nil && enableOptionalEncryption {
+				return encrypted, encrypted, nil
+			}
+			if err != nil {
+				return "", "", apperrors.ErrDecryption
 			}
 		}
 	} else if enableCompressToken {
